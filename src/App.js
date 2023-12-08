83869d1c278ac6a3efc7aececf00e21108d2ca9c
namsan01 <namsan2934@gmail.com>
2023-12-08 14:50:55 +0900

    docs: 최초 프로젝트 생성

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "@emotion/styled";
+import React from "react";
+import { Route, Routes } from "react-router-dom";
 
-function App() {
+const App = () => {
+  const Wrap = styled.div`
+    position: relative;
+    max-width: ${props => props.maxw + "px"};
+  `;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrap maxw={960}>
+      <Routes>
+        <Route path="*" element={<h1>파일이 없네요.</h1>}></Route>
+      </Routes>
+    </Wrap>
   );
-}
+};
 
 export default App;
