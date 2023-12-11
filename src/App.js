87962fe22c929d9701978d07395e257ba9d68e00
eunjin40 <dev.eunjin40@gmail.com>
2023-12-11 15:39:35 +0900

    feat : footer.js, feedlist.js 컴포넌트 생성

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import styled from "@emotion/styled";
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Link, Route, Routes } from "react-router-dom";
+import Footer from "./components/Footer";
+import FeedList from "./components/FeedList";
 
 const App = () => {
   const Wrap = styled.div`
     position: relative;
     max-width: ${props => props.maxw + "px"};
   `;
+
   return (
-    <Wrap maxw={960}>
+    <Wrap maxw={1024}>
       <Routes>
         <Route path="*" element={<h1>파일이 없네요.</h1>}></Route>
       </Routes>
+      <FeedList></FeedList>
+      <Footer></Footer>
     </Wrap>
   );
 };
