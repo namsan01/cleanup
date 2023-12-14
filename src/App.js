05fbbbe6b77d0292a9e52eced0ce13c668da3c05
Phowebing <phowebing@gmail.com>
2023-12-14 16:55:33 +0900

    feat : ErrorPage 구조잡기

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import { Link, Route, Routes } from "react-router-dom";
 import DiaryAdd from "./components/DiaryAdd";
 import FeedList from "./components/FeedList";
 import Footer from "./components/Footer";
-import List from "./components/MainList";
+import MainList from "./components/MainList";
 import Header from "./components/Header";
 import Confirm from "./components/Confirm";
 import SubBar from "./components/SubBar";
 import CreateEditList from "./components/CreateEditList";
+import ErrorPage from "./pages/ErrorPage";
 
 const App = () => {
   const Wrap = styled.div`
@@ -19,9 +20,11 @@ const App = () => {
   return (
     <Wrap maxw={1024}>
       <SubBar></SubBar>
+      <ErrorPage></ErrorPage>
       <Routes>
         <Route path="*" element={<h1>파일이 없네요.</h1>}></Route>
       </Routes>
+      <MainList></MainList>
       <FeedList></FeedList>
       <Confirm></Confirm>
       <Footer></Footer>
