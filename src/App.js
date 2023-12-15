8c88049a492a3242809898a4abb7c0f458ecf8c2
namsan01 <namsan2934@gmail.com>
2023-12-15 17:59:13 +0900

    docs: Main page 초기작업

--- src/App.js
+++ src/App.js
@@ -6,54 +6,20 @@ import Confirm from "./components/Confirm";
 import SubBar from "./components/SubBar";
 import ErrorPage from "./pages/ErrorPage";
 import Header from "./components/Header";
+import CreateEditList from "./components/CreateEditList";
+import Main from "./pages/Main";
+import { Wrap } from "./components/Common";
 
 const App = () => {
-  const Wrap = styled.div`
-    position: relative;
-    max-width: ${props => props.maxw + "px"};
-  `;
+  
 
-  const ok1 = () => {
-    alert("아니오");
-  };
-
-  const ok2 = () => {
-    alert("네");
-  };
   return (
     <Wrap maxw={1024}>
-      {/* <SubBar></SubBar>
-      <ErrorPage></ErrorPage> */}
-      <Header text="메인화면"></Header>
       <Routes>
-        {/* <Route path="/diary/add" element={<DiaryAdd />}></Route> */}
+        <Route path="/main" element={<Main />}></Route>
+        <Route path="/diary/add" element={<DiaryAdd />}></Route>
         <Route path="*" element={<h1>파일이 없네요.</h1>}></Route>
       </Routes>
-
-      {/* <Confirm
-        type={1}
-        txt={
-          <>
-            <span>저장</span>하고 나가시겠습니까?
-          </>
-        }
-        onOk={ok1}
-      ></Confirm>
-      <Confirm
-        txt={
-          <>
-            작성을 <span>취소</span> 하시겠습니까?
-          </>
-        }
-        onOk={ok2}
-      ></Confirm>
-      <Confirm
-        txt={
-          <>
-            수정을 <span>취소</span> 하시겠습니까?
-          </>
-        }
-      ></Confirm> */}
     </Wrap>
   );
 };
