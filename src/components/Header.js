6db94fae38c5dc65b6b07dd9bb6de43055edaaa5
namsan01 <namsan2934@gmail.com>
2023-12-19 18:04:04 +0900

    docs: Header.js 및 Main.js 수정

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,33 +1,43 @@
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { Topbar } from "../styles/headerstyle";
 import { useState } from "react";
 import SubBar from "./SubBar";
+import Confirm from "./Confirm";
 const Header = ({ text, type }) => {
+  const location = useLocation();
+  const pathname = location.pathname;
+  // console.log(pathname);
+  const [isModal, setIsModal] = useState(false);
+
+  const haldeClickBt = () => {
+    // console.log(pathname);
+    if (pathname === "/diary/add") {
+      setIsModal(true);
+    }
+  };
+
   const navigate = useNavigate();
   const handleClickSubBar = () => {
     navigate("/sub");
   };
+
   // isPopupOpen: 현재 팝업의 열림/닫힘 상태를 나타내는 상태
   // setPopupOpen: isPopupOpen 상태를 갱신하는 함수
-  const [isPopupOpen, setPopupOpen] = useState();
+  const [isPopupOpen, setPopupOpen] = useState(false);
 
   const handlePopupToggle = () => {
     // setPopupOpen 함수를 사용하여 isPopupOpen 상태를 반전시킴
     setPopupOpen(!isPopupOpen);
   };
 
-  const confirmBt = () => {
-    {
-      isPopupOpen && <SubBar />;
-    }
-  };
   return (
     <Topbar>
       <div className="header-wrap">
         <div className="header-left">
           <button
             disabled={type === 1}
-            onClick={type !== 1 ? confirmBt : undefined}
+            // onClick={type !== 1 ? confirmBt : undefined}
+            onClick={() => haldeClickBt()}
           >
             {type === 1 ? (
               <img
@@ -51,6 +61,7 @@ const Header = ({ text, type }) => {
           </button>
         </div>
       </div>
+      {isModal && <Confirm />}
     </Topbar>
   );
 };
